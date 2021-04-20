f0f87cfc3c37ea8c06831c7cc8dbb7dd5aa05d28
Yash Indane <yashindane46@gmail.com>
2021-04-20 15:23:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import cv2
 import boto3
-import numpy as np
 import subprocess
 
 #click photo
@@ -9,26 +8,25 @@ cap = cv2.VideoCapture(0)
 cap.set(3 , 1920)
 cap.set(4 , 1080)
 
-ret , photo = cap.read() 
+ret , photo = cap.read()
 
-cv2.imwrite("page.png",photo)
+#name of captured image file
+file_name = ""
 
-#photo = cv2.imread('page.png', cv2.IMREAD_GRAYSCALE)
-
-cv2.imwrite("page.png",photo)
+cv2.imwrite(file_name,photo)
 
 print(ret)
 cap.release()
 
 #upload photo in s3
+
 #enter s3 bucket name
 bucket_name = "" 
-file_name = "page.png"
 
 s3 = boto3.resource("s3") 
 s3.Bucket(bucket_name).upload_file(file_name , file_name)
 
-# Call Amazon Textract
+#Call Amazon Textract
 textract = boto3.client('textract')
 
 response = textract.detect_document_text(
@@ -42,12 +40,12 @@ response = textract.detect_document_text(
     }
 )
 
-
+#Writing commands to the docker file from Textract response
 with open('Dockerfile' , 'a' ) as dockerfile:
 
     for blocks in response['Blocks']:
       if blocks['BlockType'] == 'LINE':
-          #print(blocks['Text'])
+          
           dockerfile.write(blocks['Text'] + ' \n')
 
       elif blocks['BlockType'] == 'WORD':
@@ -55,10 +53,11 @@ with open('Dockerfile' , 'a' ) as dockerfile:
 
     dockerfile.close()
 
-
 #build image
-#enter container image name
+
+#container image name (username/repo:version)
 image_name = ''
+
 build_image_status = subprocess.getstatusoutput(f'docker build -t {image_name} .')
 print('image build!' if build_image_status[0] == 0 else 'error building image')
 
@@ -67,20 +66,34 @@ push_image = subprocess.getstatusoutput(f'docker push {image_name}')
 print('image pushed to DockerHub' if push_image[0] == 0 else 'error pushing image')
 
 #creating deployment
+
 #Kubernetes master DNS
-instance = ''
+master_DNS = ''
+
+#Kubernetes master key path (.pem file)
+key = ''
 
 #Kubernetes deployment name
 deployment_name = ''
 
-#Kubernetes master key (.pem file)
-master_key = ''
 deployment_status = subprocess.getstatusoutput(
-    f'ssh -i {master_key} {instance} sudo kubectl create deployment {deployment_name} --image {image_name} -- sleep infinity'
-    )
+    f'ssh -i "{key}" {master_DNS} sudo kubectl create deployment {deployment_name} --image {image_name}'
+)
+
+#exposing the deployment
+
+#port at which the service inside container is running
+port_no=
+
+#type of service
+TYPE=''
+
+service_status = subprocess.getstatusoutput(
+    f'ssh -i "{key}" {master_DNS} sudo kubectl expose deployment {deployment_name} --port={port_no} --type={TYPE}'
+)    
     
 print(
        'Deployment successful!' 
        if deployment_status[0] == 0 else
        'error'
-     )
+)     
